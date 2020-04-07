b1390d4f20a3f9c1cfff22bfeef074ecfbb560ed
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-04-06 21:53:12 -0300

    commit
    - agregar push wall en wall slide

--- game/Player/States/Wall.gd
+++ game/Player/States/Wall.gd
@@ -5,9 +5,10 @@ Este estado maneja lo que respecta al jugador cuando choca contra la pared
 
 #### export variables
 export var slide_acceleration: float = 800.0
-export var max_slide_speed: float = 200.0
+export var default_max_slide_speed: float = 200.0
 export(float, 0.05, 0.95) var friction_wall: float = 0.15
 export var wall_jump_strength: Vector2 = Vector2(300.0, 250.0)
+export(float, 1.1, 1.5) var slide_speed_incrementor = 1.3
 """
 slide_acceleration = si la velocidad de caida al tocar la pared es menor (ya esta
 	bajando por la pared por ejemplo) va acelerando su caida
@@ -22,11 +23,12 @@ jump_wall_strength = la fuerza con la que el jugador se impulsa desde la pared.
 """
 #### onready variables
 onready var move: = get_parent()
+onready var max_slide_speed = default_max_slide_speed
 
 #### variables
 var _wall_normal: float = -1.0
 var _velocity: Vector2 = Vector2.ZERO
-
+var _pushing_against_wall: bool = true
 
 #### Metodos
 func unhandled_input(event: InputEvent) -> void:
@@ -34,6 +36,20 @@ func unhandled_input(event: InputEvent) -> void:
 		jump()
 
 func physics_process(delta: float) -> void:
+	if sign(_wall_normal) > 0.0 and Input.is_action_pressed("aim_joy_left"):
+		_pushing_against_wall = true
+	elif sign(_wall_normal) < 0.0 and Input.is_action_pressed("aim_joy_right"):
+		_pushing_against_wall = true
+	else:
+		_pushing_against_wall = false
+	
+	
+	if not(_pushing_against_wall):
+		max_slide_speed = default_max_slide_speed * slide_speed_incrementor
+	else:
+		max_slide_speed = default_max_slide_speed
+
+	
 	if _velocity.y > max_slide_speed:
 		_velocity.y = lerp(_velocity.y, max_slide_speed, friction_wall)
 	else:
