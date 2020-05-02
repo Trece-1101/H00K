81bf68a1eb7c36cc9b1922825ca7a4f5bde2867c
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-05-02 20:27:21 -0300

    commit
    algunos fixes

--- game/Objects/Door.gd
+++ game/Objects/Door.gd
@@ -4,6 +4,7 @@ class_name Door
 onready var close_door := false
 
 func _on_body_entered(body: Node) -> void:
-	if not close_door:
-		close_door = true
-		$AnimationPlayer.play("close")
+	if body.name == "Player":
+		if not close_door:
+			close_door = true
+			$AnimationPlayer.play("close")
