f9f93abd81eafcb3121b401204af85bf2c6c725f
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-05-07 11:50:09 -0300

    UH016
    UH016
    - agregado codigo al boton de Salir, Opciones y Creditos
    - Agregada escenas (solo la raiz, nada mas) de Opciones y Creditos
    - Agregado textureRect para el fondo

--- game/UI/Menus/Buttons/MenuButton.gd
+++ game/UI/Menus/Buttons/MenuButton.gd
@@ -1,5 +1,7 @@
 extends Button
 
+export(String) var next_scene
+
 func _ready() -> void:
 	$Sprite.visible = false
 
@@ -11,3 +13,8 @@ func _on_mouse_exited() -> void:
 	$AnimationPlayer.play("off_hover")
 	#$Sprite.visible = false
 
+func _on_button_up() -> void:
+	if next_scene == "exit":
+		get_tree().quit()
+	else:
+		get_tree().change_scene(next_scene)
