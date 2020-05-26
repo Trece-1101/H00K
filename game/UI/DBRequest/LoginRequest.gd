add7dba5441adf37afb689972ec54e5cbac1dd08
Trece-1101 <omar.bazzi.sf@gmail.com>
2020-05-26 10:34:45 -0300

    UH026
    - Fix respuestas distintas a 200 o 401

--- game/UI/DBRequest/LoginRequest.gd
+++ game/UI/DBRequest/LoginRequest.gd
@@ -2,25 +2,26 @@ extends Node
 
 signal done
 
-onready var LoginRequest = get_node("LoginRequest")
-onready var LoginResult
+onready var LoginRequest := get_node("LoginRequest")
+onready var LoginResult = []
 
 
-func _ready():
+func _ready() -> void:
 	LoginRequest.connect("request_completed", self, "_LoginRequest_request_completed")
 
-func Login(user :String, password :String):
+func Login(user :String, password :String) -> void:
 	var headers = ["Content-Type: application/json"]
 	var query = JSON.print({"nickname": user, "password": password})
-	LoginRequest.request("http://142.93.201.7:3000/login",headers,false,HTTPClient.METHOD_POST,query)
+	LoginRequest.request("http://142.93.201.7:3000/login", headers, false, HTTPClient.METHOD_POST, query)
 
-func _LoginRequest_request_completed(_result, response_code, _headers, body):
+func _LoginRequest_request_completed(_result, response_code, _headers, body) -> void:
 	var json = JSON.parse(body.get_string_from_utf8())
 	if response_code == 200:
-		LoginResult = [true, json.result['Tipo']]
+		LoginResult = [response_code, json.result['Tipo']]
 	elif response_code == 401:
-		LoginResult = [false, json.result]
+		LoginResult = [response_code, json.result]
 	else:
 		var msj = "Error %s on database connection" % response_code
 		LoginResult = [response_code, msj]
+	
 	emit_signal("done")
--- game/UI/Menus/LoginMenu.gd
+++ game/UI/Menus/LoginMenu.gd
@@ -10,8 +10,6 @@ onready var BDRequest := $LoginRequest
 
 func _ready() -> void:
 	user.grab_focus()
-	#$Error.show()
-	#$Error/ColorRect/Label.text = OS.get_name()
 
 func load_next_scene() -> void:
 	get_tree().change_scene(next_scene)
@@ -21,33 +19,34 @@ func _on_Salir_button_down() -> void:
 
 func _on_Enter_button_down() -> void:
 	BDRequest.Login(user.text, password.text)
-	yield(BDRequest,"done")
+	$Searching.show()
+	toggle_insert(false)
+	yield(BDRequest, "done")
 	var result = BDRequest.LoginResult
-	if result[0]:
+	#print(result)
+	if result[0] == 200:
 		## ACA DEVUELVE EL TIPO DE USUARIO Q ES Y DEBERIA SER GUARDADO EN UNA VARIABLE GLOBAL DEL JUEGO
 		## tipoUser = result[1] 
 		pop_up_show($OK)
 		ok_answer = true
 	else:
 		$Error/ColorRect/Label.text = result[1]
+		if result[0] != 401:
+			$Error/ColorRect/Label.text = "Error conexion"
 		pop_up_show($Error)
 		ok_answer = false
-	
-	#if user.text == "omar13" and password.text == "passomar13":
-	#	pop_up_show($OK)
-	#	ok_answer = true
-	#else:
-	#	pop_up_show($Error)
-	#	ok_answer = false
-
 
 func pop_up_show(popup: Popup) -> void:
+	$Searching.hide()
 	popup.show()
 	$Timer.start()
-	user.editable = false
-	password.editable = false
-	$LogPanel/ColorRect/Salir.disabled = true
+	toggle_insert(false)
 
+func toggle_insert(value: bool) -> void:
+	user.editable = value
+	password.editable = value
+	$LogPanel/ColorRect/Salir.disabled = not value
+	$LogPanel/ColorRect/Enter.disabled = not value
 
 func _on_Timer_timeout() -> void:
 	if ok_answer:
@@ -56,8 +55,6 @@ func _on_Timer_timeout() -> void:
 		$Error.hide()
 		user.text = ""
 		password.text = ""
-		user.editable = true
-		password.editable = true
-		$LogPanel/ColorRect/Salir.disabled = false
+		toggle_insert(true)
 		user.grab_focus()
 
